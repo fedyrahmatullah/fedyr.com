379924667b8aed8231b339482906dec463d2e156
fedyrahmatullah <48233176+fedyrahmatullah@users.noreply.github.com>
2024-08-09 20:43:31 +0700

    Update header.php

--- public/header.php
+++ public/header.php
@@ -6,7 +6,7 @@
     <meta name="author" content="Fedy Rahmatullah">
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <link rel="stylesheet" href="/public/assets/css/style.css" />
+    <link rel="stylesheet" href="assets/css/style.css" />
     <link rel="stylesheet" href="assets/css/mediaqueries.css" />
     <link rel="stylesheet" href="assets/css/project-style.css" />
     <!-- Include Font Awesome -->
