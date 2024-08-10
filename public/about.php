ed2c34e697c0b74e1d8b5d56438a4427a316aa49
fedyrahmatullah <48233176+fedyrahmatullah@users.noreply.github.com>
2024-08-11 06:33:19 +0700

    edit: section about and css

--- public/about.php
+++ public/about.php
@@ -4,232 +4,45 @@
   <!-- End Header -->
  
 
-    <!-- about -->
-    <section id="about">
-      <p class="section__text__p1">Get To Know More</p>
-      <h1 class="title">About Me</h1>
-      <div class="section-container">
-        <div class="section__pic-container">
-          <img
-            src="./assets/img/fedy-profilpict.jpg"
-            alt="Profile picture"
-            class="about-pic"
-          />
-        </div>
-        <div class="about-details-container">
-          <div class="about-containers">
-            <div class="details-container">
-              <img
-              src="./assets/img/education.png"
-              alt="Education icon"
-              class="icon"
-            />
-              <h3>Fedy Rahmatullah</h3>
-              <p>He/Him   •   Indonesia</p>
-              <p >
-                <span class="badge badge-pill badge-primary">Indonesian</span>
-                <span class="badge badge-pill badge-secondary">IT and Data Enthusiast</span>
-                <span class="badge badge-pill badge-success">Data Visualization</span>
-                <span class="badge badge-pill badge-danger">Data Analyst</span>
-                <span class="badge badge-pill badge-warning">Web Developer</span>
-                <span class="badge badge-pill badge-info">Reading</span>
-                <span class="badge badge-pill badge-light">Music</span>
-                <span class="badge badge-pill badge-dark">Movie</span>
-              </p>
-            </div>
-            <div class="details-container">
-              <img
-              src="./assets/img/experience.png"
-              alt="Experience icon"
-              class="icon"
-            />
-              <h3>Education</h3>
-              <p><b>Politeknik Negeri Jember</b><br />Bachelor of Applied Computer, Informatics Engineering</p>
-            </div>
-          </div>
-          <div class="text-container">
-            <p >
-              An IT and data enthusiast who graduated from <a target="_blank" style="color:black;" href="https://polije.ac.id/">Polytechnic of Jember</a> 
-              with a Bachelor's degree in Informatics Engineering and has a great passion 
-              for a career in IT and other fields of work.
-            </p>
-            <br>
-            <p>One of my strengths is that I am a good planner when
-              it comes to achieving my targets, and I have a huge
-              desire to learn new things. It can be proven by several
-              organizations that I joined during my college life as
-              Head of the Information Systems Division at HMJ TI
-              State Polytechnic of Jember and Coordinator of the
-              Informatics Engineering Students of 2017 (as known as
-              “Ketua Angkatan”). I did that by staying focused on
-              college and graduating on time with a 3,82 GPA.</p>
-          </div>
-        </div>
-      </div>
-
-    </section>
-    
-    <section id="experience">
-      <p class="section__text__p1">Explore My</p>
-      <h1 class="title">Experience</h1>
-      <div class="experience-details-container">
-        <div class="about-containers">
-          <div class="details-container">
-            <h2 class="experience-sub-title">Tools</h2>
-            <div class="article-container">
-              <article>
-                <img
-                  src="./assets/img/checkmark.png"
-                  alt="Experience icon"
-                  class="icon"
-                />
-                <div>
-                  <h3>Tableau</h3>
-                  <!-- <p>Experienced</p> -->
-                </div>
-              </article>
-              <article>
-                <img
-                  src="./assets/img/checkmark.png"
-                  alt="Experience icon"
-                  class="icon"
-                />
-                <div>
-                <h3>GitHub</h3>
-                  <!-- <p>Experienced</p> -->
-                </div>
-              </article>
-              <article>
-                <img
-                  src="./assets/img/checkmark.png"
-                  alt="Experience icon"
-                  class="icon"
-                />
-                <div>
-                  <h3>Jupyter Notebook</h3>
-                  <!-- <p>Intermediate</p> -->
-                </div>
-              </article>
-              <article>
-                <img
-                  src="./assets/img/checkmark.png"
-                  alt="Experience icon"
-                  class="icon"
-                />
-                <div>
-                  <h3>Google BigQuery</h3>
-                  <!-- <p>Basic</p> -->
-                </div>
-              </article>
-              <article>
-                <img
-                  src="./assets/img/checkmark.png"
-                  alt="Experience icon"
-                  class="icon"
-                />
-                <div>
-                  <h3>Adobe Illustrator</h3>
-                  <!-- <p>Basic</p> -->
-                </div>
-              </article>
-              <article>
-                <img
-                  src="./assets/img/checkmark.png"
-                  alt="Experience icon"
-                  class="icon"
-                />
-                <div>
-                  <h3>Google Spreadsheet</h3>
-                  <!-- <p>Intermediate</p> -->
-                </div>
-              </article>
-            </div>
-          </div>
-          <div class="details-container">
-            <h2 class="experience-sub-title">Skills</h2>
-            <div class="article-container">
-              <article>
-                <img
-                  src="./assets/img/checkmark.png"
-                  alt="Experience icon"
-                  class="icon"
-                />
-                <div>
-                  <h3>Data Analysis</h3>
-                  <!-- <p>Basic</p> -->
-                </div>
-              </article>
-              <article>
-                <img
-                  src="./assets/img/checkmark.png"
-                  alt="Experience icon"
-                  class="icon"
-                />
-                <div>
-                  <h3>Data Visualization</h3>
-                  <!-- <p>Intermediate</p> -->
-                </div>
-              </article>
-              <article>
-                <img
-                  src="./assets/img/checkmark.png"
-                  alt="Experience icon"
-                  class="icon"
-                />
-                <div>
-                  <h3>Web Development</h3>
-                  <!-- <p>Intermediate</p> -->
-                </div>
-              </article>
-              <article>
-                <img
-                  src="./assets/img/checkmark.png"
-                  alt="Experience icon"
-                  class="icon"
-                />
-                <div>
-                  <h3>Graphic Design</h3>
-                  <!-- <p>Intermediate</p> -->
-                </div>
-              </article>
-            </div>
-          </div>
-        </div>
-      </div>
-      <img
-        src="./assets/img/arrow.png"
-        alt="Arrow icon"
-        class="icon arrow"
-        onclick="location.href='#contact'"
-      />
-    </section>
-
-
-    <!-- contact -->
-    <section id="contact">
-      <p class="section__text__p1">Get in Touch</p>
-      <h1 class="title">Contact Me</h1>
-      <div class="contact-info-upper-container">
-        <div class="contact-info-container">
-          <img
-            src="./assets/img/email.png"
-            alt="Email icon"
-            class="icon contact-icon email-icon"
-          />
-          <p><a href="mailto:fedy.rahmatullah@gmail.com">fedy.rahmatullah@gmail.com</a></p>
-        </div>
-        <div class="contact-info-container">
-          <img
-            src="./assets/img/linkedin.png"
-            alt="LinkedIn icon"
-            class="icon contact-icon"
-          />
-          <p><a target="_blank" href="https://www.linkedin.com/in/fedyrahmatullah/">LinkedIn</a></p>
-        </div>
-      </div>
+  <section id="about">
+    <p class="section__text__p1">Get To Know More</p>
+    <h1 class="title">About Me</h1>
+    <br>
+    <div class="about-pic-container">
+      <img src="./assets/img/fedy-profilpict.jpg" alt="Profile picture" class="about-pic">
+    </div>
+    <br>
+    <div class="about-text-container">
+      <p>
+        I am an IT and data enthusiast with a deep passion for turning data into actionable insights. I graduated with a Bachelor's degree in Informatics Engineering from <a target="_blank" style="color:black;" href="https://polije.ac.id/">Politeknik Negeri Jember</a>, earning a GPA of 3.82. My interests lie in data analysis and visualization, particularly using tools like Tableau. I am constantly learning and seeking new ways to enhance my skills.
+      </p>
+      <br>
+      <p>
+        During my time in college, I was actively involved in several organizations, serving as the Head of the Information Systems Division at HMJ TI State Polytechnic of Jember and as the Coordinator for the Informatics Engineering Students of 2017, also known as the "Ketua Angkatan." These experiences have shaped my approach to problem-solving and fueled my drive to continuously grow in the field of data.
+      </p>
+    </div>
+  </section>
+  <br>
+  <br>
+  <br>
+  <br>
+  <br>
+  <br>
   
-    </section>
-
+  <section id="contact">
+    <p class="section__text__p1">Get in Touch</p>
+    <h1 class="title">Contact Me</h1>
+    <div class="contact-info-upper-container">
+      <div class="contact-info-container">
+        <img src="./assets/img/email.png" alt="Email icon" class="icon contact-icon email-icon">
+        <p><a href="mailto:fedy.rahmatullah@gmail.com">fedy.rahmatullah@gmail.com</a></p>
+      </div>
+      <div class="contact-info-container">
+        <img src="./assets/img/linkedin.png" alt="LinkedIn icon" class="icon contact-icon">
+        <p><a target="_blank" href="https://www.linkedin.com/in/fedyrahmatullah/">LinkedIn</a></p>
+      </div>
+    </div>
+  </section>
 
 
 
