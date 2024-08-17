377a830d19a023ba0a47f43eee9d103513fe5bc1
fedyrahmatullah <48233176+fedyrahmatullah@users.noreply.github.com>
2024-08-17 21:20:06 +0700

    set width

--- public/footer.php
+++ public/footer.php
@@ -1,5 +1,5 @@
 <footer >
-      <p>Copyright &#169; <b>2024 Fedy Rahmatullah.</b> All Rights Reserved.</p>
+    <p>Copyright &#169; <b>2024 Fedy Rahmatullah.</b> All Rights Reserved.</p>
 </footer>
     <script src="assets/js/script.js"></script>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
