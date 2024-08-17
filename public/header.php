f9a3066aa943e4617df8d1be460a34f11d3c05e0
fedyrahmatullah <48233176+fedyrahmatullah@users.noreply.github.com>
2024-08-17 23:36:31 +0700

    chore : add meta tags

--- public/header.php
+++ public/header.php
@@ -15,6 +15,27 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
     <link href="assets/img/favicon.png" rel="icon">
     <title><?php echo isset($title) ? $title : 'Default Title'; ?></title>
+
+
+    <meta property="og:title" content="Fedy Rahmatullah - IT Enthusiast & Data Analyst | Portfolio">
+    <meta property="og:description" content="Explore the personal website of Fedy Rahmatullah, an IT enthusiast and data analyst. Discover projects, experience, and insights into data analysis.">
+    <meta property="og:image" content="https://fedyr.com/assets/img/favicon.png">
+    <meta property="og:url" content="https://fedyr.com">
+    <meta property="og:type" content="website">
+    <meta name="twitter:card" content="summary_large_image">
+    <meta name="twitter:title" content="Fedy Rahmatullah - IT Enthusiast & Data Analyst | Portfolio">
+    <meta name="twitter:description" content="Explore the personal website of Fedy Rahmatullah, an IT enthusiast and data analyst. Discover projects, experience, and insights into data analysis.">
+    <meta name="twitter:image" content="https://fedyr.com/assets/img/favicon.png">
+    <script type="application/ld+json">
+    {
+      "@context": "https://schema.org",
+      "@type": "Person",
+      "name": "Fedy Rahmatullah",
+      "description": "IT Enthusiast and Data Analyst. Personal website showcasing projects and experience.",
+      "url": "https://fedyr.com",
+      "image": "https://fedyr.com/assets/img/favicon.png"
+    }
+    </script>
 </head>
 <body>
     <nav id="desktop-nav">
